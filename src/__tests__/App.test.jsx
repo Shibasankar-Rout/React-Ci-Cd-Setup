506408e38100775d057a29dde4e7022d16ac6910
Shibasankar Rout <shibasankar@Shibasankars-MacBook-Air.local>
2025-04-29 12:39:32 +0530

    added codes

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,7 @@
+// src/__tests__/App.test.js
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders Vite + React text', () => {
+  render(<App />);
+});
